17da7c945704b720e23e958b62118774cfde5a68
Sheharyar Ahmed <sheryahmedme1@gmail.com>
2025-09-16 01:05:43 +0500

    building components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,71 @@
 import React from "react"; //adding the libraries
 import ReactDOM from "react-dom/client";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   //the component
-  return <h1>HELLO REACT</h1>;
+  return (
+    <div>
+      <h1>HELLO REACT</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+//another component but it is not being rendered as the App component is being rendered as written below but its ui can be render as it is declared in the App component
+function Pizza() {
+  return (
+    <div>
+      <img src="../pizzas/funghi.jpg" alt="Pizza Funghi"></img>
+      <h2>Pizza Funghi</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
